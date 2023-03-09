d2bdbaf20aed6107bb16253c752c1108c2e8bb90
root <rogerpablo55@hotmail.com>
2023-03-09 16:44:48 -0300

    create dynamoDB

--- aws/iac-terraform-aws-s3/main.tf
+++ aws/iac-terraform-aws-s3/main.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>4.33.0"
+      version = "~>4.57.0"
     }
   }
 }
@@ -22,15 +22,14 @@ data "aws_caller_identity" "current" {}
 
 ##############################BucketS3#####################################
 # Create a bucket for storage the terraform remote-state
-resource "aws_s3_bucket" "example" {
-  bucket = "my-example-bucket-roger"
+resource "aws_s3_bucket" "remote-state" {
+  bucket = "my-example-roger-pablo"
   tags   = var.resource_tags
 }
 
-#block public access
+#block whole  public acess possibilities
 resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
-
+  bucket = aws_s3_bucket.remote-state.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
@@ -39,8 +38,8 @@ resource "aws_s3_bucket_public_access_block" "example" {
 
 
 #Encrypt bucket With SSE-S3
-resource "aws_s3_bucket_server_side_encryption_configuration" "eyxample" {
-  bucket = aws_s3_bucket.example.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "remote-state" {
+  bucket = aws_s3_bucket.remote-state.bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -49,18 +48,18 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "eyxample" {
 }
 
 #Active versioning on bucket
-resource "aws_s3_bucket_versioning" "example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_versioning" "remote-state" {
+  bucket = aws_s3_bucket.remote-state.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 #Lifecycle for version archives 
-resource "aws_s3_bucket_lifecycle_configuration" "example" {
+resource "aws_s3_bucket_lifecycle_configuration" "remote-state" {
   # Must have bucket versioning enabled first
-  depends_on = [aws_s3_bucket_versioning.example]
-  bucket     = aws_s3_bucket.example.bucket
+  depends_on = [aws_s3_bucket_versioning.remote-state]
+  bucket     = aws_s3_bucket.remote-state.bucket
   rule {
     id = "config"
     noncurrent_version_expiration {
@@ -73,25 +72,33 @@ resource "aws_s3_bucket_lifecycle_configuration" "example" {
 
 # output bucket remote state
 output "remote_state_bucket" {
-  value = aws_s3_bucket.example.bucket
+  value = aws_s3_bucket.remote-state.bucket
 }
 
 # Output bucket arn 
 output "remote_state_bucket_arn" {
-  value = aws_s3_bucket.example.arn
+  value = aws_s3_bucket.remote-state.arn
 }
 
 #################DynamoDB############################
 #Active lock in on terraform.tfstate and prevent for doble access.
 resource "aws_dynamodb_table" "terraform-lock" {
-  name           = "my-example-bucket-roger"
+  name           = "my-example-roger-pablo"
   read_capacity  = 5
   write_capacity = 5
   hash_key       = "LockID"
+  
+  server_side_encryption {
+    enabled = true
+  }
+  point_in_time_recovery {
+    enabled = true
+  }
   attribute {
     name = "LockID"
     type = "S"
   }
-  tags = var.resource_tags
+  tags = {
+    "Name" = "DynamoDB Terraform State Lock Table"
+  }
 }
-
